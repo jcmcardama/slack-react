cd96dc5e2200b04a19ca6f05d0a89b81d1404e8f
Jan Carlo Cardama <97457413+jcmcardama@users.noreply.github.com>
2022-05-28 12:18:19 +0800

    Update api-login.js

--- src/api/api-login.js
+++ src/api/api-login.js
@@ -1,28 +1,3 @@
-//LOGIN AND REGISTRATION
-
-//REGISTRATION
-export const userRegistration = (email, password, passwordConfirmation) => {
-    const formData = new FormData();
-    formData.append("email", email);
-    formData.append("password", password);
-    formData.append("password_confirmation", passwordConfirmation);
-
-    var requestOptions = {
-    method: 'POST',
-    body: formData,
-    redirect: 'follow'
-    };
-
-    fetch("http://206.189.91.54//api/v1/auth/", requestOptions)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-};
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-};
-
 //LOGIN
 export const userLogin = async (email, password) => {
     let loginHeaders = new Headers()
