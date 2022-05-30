16a62e539674a85cadf3b7497ec4c64fba3f0ac3
Jan Carlo Cardama <jcmcardama@gmail.com>
2022-05-30 19:44:17 +0800

    update login api and reg api

--- src/api/api-login.js
+++ src/api/api-login.js
@@ -1,21 +1,24 @@
 //LOGIN
-export const userLogin = async (email, password) => {
-    let loginHeaders = new Headers()
-    let raw = {
-        email: email,
-        password: password
-    }
+export const UserLogin = (email, password) => {
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
 
-    const options = {
+    var requestOptions = {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(raw)
-
-    }
-    //[WIP]
-    await fetch('http://206.189.91.54//api/v1/auth/sign_in', options)
-            .then((response) => console.log(response.headers.get('access-token')))
-
+        body: formData,
+        redirect: 'follow'
+    };
+        
+    fetch("http://206.189.91.54//api/v1/auth/sign_in", requestOptions)
+        .then(response => {
+            console.log(response.headers.get('access-token'));
+            console.log(response.headers.get('client'));
+            console.log(response.headers.get('expiry'));
+            console.log(response.headers.get('uid'));
+        })
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => console.log('error', error));
 }
