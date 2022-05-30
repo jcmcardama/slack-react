1cb4c6c91999aa306a4527de4ea9778a53fd95e1
Jan Carlo Cardama <jcmcardama@gmail.com>
2022-05-30 21:46:07 +0800

    update everything

--- src/App.js
+++ src/App.js
@@ -1,92 +1,13 @@
 import './App.css';
-import {useState} from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from "react-router-dom";
-
-// User Registration
-// -needs email 
-// -password
-// -password confirmation
-
-// Login
-// -needs email
-// -needs password
-
-// Send Message
-// -receiver id
-// -receiver class
-// -body
-
-// Retrieve Messages
-// -sender id 
-// -receiver class 
-// -receiver id 
-
-// Create Channel with members
-// -name of channel 
-// -user id of creator 
-
-// Get all users channels
-
-
-// Get channel details via channel ID
-
-// Add member to a channel
-// -id 
-// -member id 
-
-// List of All Users
-// Get all users owned channels
-// Recently DMs
-
-function Signin() {
-  return (
-    <></>
-  )
-}
-
-function CreateAccount() {
-  return (
-    <></>
-  )
-}
-
-function Dashboard() {
-  return (
-    <></>
-  )
-}
+import SendMessage from './component/SendMessage.js';
+import Login from './component/Login.js';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/signin">Signin</Link>
-            </li>
-            <li>
-              <Link to="/get-started">Create an account</Link>
-            </li>
-            <li>
-              <Link to="/client">Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Switch>
-        <Route path="/get-started" component={CreateAccount} />
-        <Route path="/client" component={Dashboard} /> 
-        <Route path="/signin" component={Signin} />
-      </Switch>
-    </Router>
+    <div>
+      <Login />
+      <SendMessage />
+    </div>
   );
 }
 
