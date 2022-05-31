46cd6e8c06c287f20dd3c10c79dde6e1ff3346c2
Jan Carlo Cardama <jcmcardama@gmail.com>
2022-05-31 20:34:46 +0800

    update apis

--- src/App.js
+++ src/App.js
@@ -1,76 +1,14 @@
 import Login from './component/Login';
-import UserRegistration from './component/UserRegistration';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
-
-// User Registration
-// -needs email 
-// -password
-// -password confirmation
-
-// Login
-// -needs email
-// -needs password
-
-// Send Message
-// -receiver id
-// -receiver class
-// -body
-
-// Retrieve Messages
-// -sender id 
-// -receiver class 
-// -receiver id 
-
-// Create Channel with members
-// -name of channel 
-// -user id of creator 
-
-// Get all users channels
-
-
-// Get channel details via channel ID
-
-// Add member to a channel
-// -id 
-// -member id 
-
-// List of All Users
-// Get all users owned channels
-// Recently DMs
+import RetrieveMessage from './component/RetrieveMessage';
+import SendMessage from './component/SendMessage';
 
 function App() {
   return (
-    <>
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/signin">Signin</Link>
-            </li>
-            <li>
-              <Link to="/get-started">Create an account</Link>
-            </li>
-            <li>
-              <Link to="/client">Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Switch>
-        <Route path="/get-started" component={UserRegistration} />
-        <Route path="/client" /> 
-        <Route path="/signin" component={Login} />
-      </Switch>
-    </Router>
-    </>
->>>>>>> 3f8b3401ce0f2bb53d0626ac1b89d8c831d0ac9e
+    <div>
+      <Login />
+      <SendMessage />
+      <RetrieveMessage />
+    </div>    
   );
 }
 
