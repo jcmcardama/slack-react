e0ab0d1402165837082831984a43d22bae584eb6
Jan Carlo Cardama <jcmcardama@gmail.com>
2022-06-02 22:01:38 +0800

    update chat title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import './App.css';
-import Login from './component/Login.js';
-import Chat from './component/Chat.js'
-// import Dashboard from './component/Dashboard';
-// import {userChannels} from './api/api-channels.js'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import Login from "./component/Login.js";
+import UserRegistration from "./component/UserRegistration.js";
+import Dashboard from "./component/Dashboard.js";
+import {userChannelNamesAndId} from "./api/api-channels";
+import {fetchRecentMessages} from './api/api-recentDm';
+
+fetchRecentMessages()
+userChannelNamesAndId()
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page cannot be found.</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <Login />
-      {/* {userChannels()} */}
-      <Chat receiverId={2118} receiverClass={"User"}/>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign in</Link>
+            </li>
+            <li>
+              <Link to="/create-account">Create Account</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/signin" component={Login} />
+          <Route path="/create-account" component={UserRegistration} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route exact path="/" component={Home} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
