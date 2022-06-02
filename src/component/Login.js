960725e1653262a21bf61f549480b6cbc5b60c08
grailwilly <grailjwi11y@gmail.com>
2022-06-02 19:14:56 +0800

    Resolve conflict

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,7 +1,8 @@
-import { useState } from "react"
-import { userLogin } from "../api/api-login.js"
-import { Route, Redirect } from "react-router-dom";
-import Dashboard from "./Dashboard";
+import { useState } from "react";
+import { userLogin } from "../api/api-login.js";
+import { FiGlobe, FiChevronDown } from "react-icons/fi";
+import { FcGoogle } from 'react-icons/fc';
+import { AiFillApple } from 'react-icons/ai';
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -26,23 +27,67 @@ const Login = () => {
     };
 
     return (
-        <div>
-        <h1>Log In</h1>
-        <form onSubmit={submitHandler}>
-            <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={emailChangeHandler} 
-            />
-            <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={passwordChangeHandler} 
-            />
-            <button type="submit">Submit</button>
-        </form>
+        <div className="auth">
+            {/* Header */}
+            <header className='auth-header'>
+                <div>
+                    <img className='slack-logo-auth' src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Slack_Technologies_Logo.svg' alt='Slack-logo' height='24px'/>
+                </div>
+                <div>
+                    <h1>Sign in to Slack</h1>
+                    <p>We suggest using the <strong>email address you use at work.</strong></p>
+                </div>
+            </header>
+            {/* Body */}
+            <form className="login-form" onSubmit={submitHandler}>
+                {/* Google Apple -- to refactor if may time */}
+                <div className="login-google">
+                    <div className="login-icon">
+                        <FcGoogle />
+                    </div>
+                    <p>Sign in with Google</p>
+                </div>
+                <div className="login-apple">
+                    <div className="login-icon">
+                        <AiFillApple />
+                    </div>
+                    <p>Sign in with Apple</p>
+                </div>
+                {/* Divider */}
+                <div className="login-divider">
+                    <p className="login-divider-text">OR</p>
+                </div>
+                <input
+                className="input-auth"
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={emailChangeHandler} required
+                />
+                <input
+                className="input-auth"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={passwordChangeHandler} 
+                required
+                />
+                <button className="auth-button" type="submit">Submit</button>
+            </form>
+            {/* Footer */}
+            <footer className='auth-footer'>
+                <div>
+                    <a href="https://slack.com/legal">Privacy & Terms</a>
+                </div>
+                <div>
+                    <a href="https://slack.com/help/contact">Contact Us</a>
+                </div>
+                <div className='change-region'>
+                    <span><FiGlobe/></span>
+                    <span>Change region</span>
+                    <span><FiChevronDown/></span>
+                </div>
+            </footer>
         </div>
     )
 }
