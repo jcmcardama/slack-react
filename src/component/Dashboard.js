0d62a91b3593e6f442692f6d01ec3067b613b74a
grailwilly <grailjwi11y@gmail.com>
2022-06-02 19:58:46 +0800

    Add channels and direct messages in dashboard component

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,8 +1,81 @@
 import { useState } from "react";
-import {listMessage} from "../api/api-retrieve-messages.js"
-import ListMessage from "./ListMessage.js";
+import {listMessage} from "../api/api-retrieve-messages.js";
 import Chat from "./Chat.js";
-import ChatTitle from "./ChatTitle.js";
+import {
+    Switch,
+    Route,
+    Link,
+    useRouteMatch
+  } from "react-router-dom";
+import {channels} from "../api/api-channels";
+import {recentMessages} from '../api/api-recentDm';
+
+
+//messages coming from the database server
+
+const Messages = () => {
+  let match = useRouteMatch();
+  
+  return (
+    <div>
+      <h2>Direct Message</h2>
+
+      <ul >
+        {recentMessages.map((message) => {
+          
+          return (
+            <li key={message.key} onClick={() => console.log(`id: ${message.id}, class: 'User'`)}>
+              <Link  to={`${match.url}/channels/${message.id}`}>
+                {" "}
+                {message.uid}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <Switch>
+        {/* <Route path={`${match.path}/:messageId`}>
+          <Messages />
+        </Route>
+        <Route path={match.path}>
+          <Chat />
+        </Route> */}
+      </Switch>
+    </div>
+  );
+};
+
+const Channels = () => {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Channels</h2>
+      <ul>
+        {channels.map((channel) => {
+          return (
+            <li key={channel.name} onClick={() => console.log(`id: ${channel.id}, class: 'Channel'`)}>
+              <Link to={`${match.url}/channels/${channel.id}`}>
+                {" "}
+                {channel.name.toUpperCase()}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <Switch>
+        {/* <Route path={`${match.path}/:channelId/:messageId`}>
+          <Messages />
+        </Route>
+        <Route path={`${match.path}/:channelId`}>
+          <Channels />
+        </Route> */}
+      </Switch>
+    </div>
+  );
+};
 
 const Dashboard = () => {
     const [receiverId, setReceiverId] = useState("");
@@ -19,18 +92,8 @@ const Dashboard = () => {
 
     return(
         <div>
-            <ChatTitle data={data}/>
-            <form onSubmit={submitHandler}>
-                <input
-                        type="textarea"
-                        placeholder="Receiver id"
-                        value={receiverId}
-                        onChange={receiverChangeHandler}
-                />
-                <button type="submit">Retrieve Message</button>
-            </form>
-            <Chat />
-            <ListMessage data={data}/>
+            <Channels />
+            <Messages />
         </div>
     );
 };
