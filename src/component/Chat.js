3eb14b6a05cb15655d684b323022381c2b03371e
Jan Carlo Cardama <jcmcardama@gmail.com>
2022-06-03 08:48:25 +0800

    update chat comp and list message layout

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,25 +1,13 @@
 import { useState } from "react";
 import { sendMessage } from "../api/api-send-message.js"
-// import { userIdByEmail } from "../api/api-users"
+import Dashboard from '../component/Dashboard';
 
 const Chat = (prop) => {
     const [message, setMessage] = useState("")
-    // const [receiverEmail, setReceiverEmail] = useState("")
 
     const messageChangeHandler = (event) => {
         setMessage(event.target.value)
     }
-
-    // const receiverChangeHandler = (event) => {
-    //     return setReceiverEmail(event.target.value)
-    // }
-
-    // const submitHandler = async (event) => {
-    //     event.preventDefault();
-    //     let receiverId = await userIdByEmail(receiverEmail)
-    //     sendMessage(receiverId, message)
-    //     setMessage("")
-    // }
 
     const submitHandler = (event) => {
         event.preventDefault();
@@ -29,10 +17,8 @@ const Chat = (prop) => {
 
     return (
         <div>
-            <h2>Send Message</h2>
+            <Dashboard receiverId={prop.receiverId} receiverClass={prop.receiverClass}/>
             <form onSubmit={submitHandler}>
-                {/* <label htmlFor="receiver-email">To:</label>
-                <input type="text" name="receiver-email" onChange={receiverChangeHandler} /> */}
                 <input
                     type="textarea"
                     placeholder="Message..."
--- src/component/ListMessage.js
+++ src/component/ListMessage.js
@@ -1,17 +1,17 @@
 const ListMessage = (prop) => {
     const items = prop.data;
+
     return (
         <div>
             {items.data !== undefined ? 
-                (
-                    items.data.map((item,index) => (
+            (
+                items.data.map((item,index,arr) => (
                     <div key={index}>
-                        {item.sender.email} {item.created_at.slice(11,-8)}: {item.body}
+                        <div>{((index > 0) && (arr[index - 1]).sender.email === (arr[index]).sender.email) ? (<></>) : (<>{item.sender.email}:</>)} 
+                        </div>{item.created_at.slice(11,-8)} {item.body}
                     </div>
-                    ))
-                ) :
-                (<></>)
-            }
+                ))
+            ) : (<></>)}
         </div>
     );
 };
